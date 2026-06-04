c00b49f385d0313dc722ae1c097e80310baebf58
Appocalypse Games <55316319+appocalypsegames@users.noreply.github.com>
2026-06-04 16:21:32 +0200

    more

--- js/stickers.js
+++ js/stickers.js
@@ -150,6 +150,12 @@ const albumConfigs = {
 		extra: [
 			"el4"
 		]
+    },
+    "monstruos-s-a-coleccion-completa": { 
+        maxNum: 180,
+		extra: [
+			"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"
+		]
     }
 };
 
